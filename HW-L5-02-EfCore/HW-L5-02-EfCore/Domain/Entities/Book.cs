58cd8665f5824b990a50cba521e77f3a71f424d6
Farzanehmohammadiha <farzaneh.mohammadi144@gmail.com>
2026-07-30 01:39:52 +0330

    Implementation of Homework6-2

---
+++ HW-L5-02-EfCore/HW-L5-02-EfCore/Domain/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_L5_02_EfCore.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Author { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public int Mojudi { get; set; }
+    }
+}
---
+++ HW-L5-02-EfCore/HW-L5-02-EfCore/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HW_L5_02_EfCore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HW_L5_02_EfCore.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
